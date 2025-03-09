b73bd47eec461309daac1dc585f89dde27663359
Joe <joegodwanggod@gamil.com>
2025-03-09 15:50:46 +0800

    v1.0

---
+++ include/Monkey.hpp
@@ -0,0 +1,92 @@
+#pragma once
+#ifndef MONKEY_HPP
+#define MONKEY_HPP
+
+#include <vector>
+#include <string>
+#include "Util/Animation.hpp"
+#include "Util/GameObject.hpp"
+#include "Attack.hpp"
+#include "Balloon.hpp"
+#include "Range.hpp"
+
+class Monkey : public Util::GameObject {
+public:
+
+      explicit Monkey(glm::vec2 position);
+      void SetPosition(const glm::vec2& Position);
+      void SetImage(const std::string& ImagePath);
+      void SetRadius(int radius);
+      void SetRotation(glm::vec2 Position);
+      void SetCd(int cd);
+      void UpdateRange();
+      void ResetCount();
+      bool IsInside(glm::vec2 mousePosition);
+      void CheckRangeVisible();
+      void SetSize(glm::vec2 size);
+
+      [[nodiscard]] virtual std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition);
+      [[nodiscard]] glm::vec2 GetPosition() const { return m_Transform.translation; }
+      [[nodiscard]] int GetRadius() const { return m_Radius; }
+      [[nodiscard]] std::shared_ptr<Range> GetRange() const { return m_Range; }
+      [[nodiscard]] virtual bool IsCollision(const std::shared_ptr<Balloon>& other) const;
+      [[nodiscard]] bool Countdown();
+
+private:
+      std::string m_ImagePath;
+      int m_Radius;
+      glm::vec2 m_Size;
+      int m_Count = 0;
+      int m_Cd;
+      std::shared_ptr<Range> m_Range = std::make_shared<Range>(m_Transform.translation, m_Radius);
+};
+
+#endif //MONKEY_HPP
+
+class DartMonkey : public Monkey {
+public:
+      explicit DartMonkey(glm::vec2 position);
+      [[nodiscard]] std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition) override;
+};
+
+class NailMonkey : public Monkey {
+public:
+      explicit NailMonkey(glm::vec2 position);
+      [[nodiscard]] std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition) override;
+};
+
+class SniperMonkey : public Monkey {
+public:
+      explicit SniperMonkey(glm::vec2 position);
+      [[nodiscard]] std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition) override;
+};
+
+class BoomerangMonkey : public Monkey {
+public:
+      explicit BoomerangMonkey(glm::vec2 position);
+      [[nodiscard]] std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition) override;
+};
+
+class NinjaMonkey : public Monkey {
+public:
+      explicit NinjaMonkey(glm::vec2 position);
+      [[nodiscard]] std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition) override;
+};
+
+class Cannon : public Monkey {
+public:
+      explicit Cannon(glm::vec2 position);
+      [[nodiscard]] std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition) override;
+};
+
+
+class Airport : public Monkey {
+public:
+      explicit Airport(glm::vec2 position);
+      [[nodiscard]] std::vector<std::shared_ptr<Attack>> ProduceAttack(glm::vec2 goalPosition) override;
+      [[nodiscard]] bool IsCollision(const std::shared_ptr<Balloon>& other) const override;
+      glm::vec2 ProduceCoordinateByAngle(glm::vec2 position, float angle);
+private:
+      int airplane_num = 1;
+      std::vector<std::shared_ptr<Attack>> m_Airplanes;
+};
---
+++ src/Monkey.cpp
@@ -0,0 +1,271 @@
+#include "Monkey.hpp"
+
+#include "Util/Logger.hpp"
+#include "Util/Input.hpp"
+#include "Util/Keycode.hpp"
+
+#define PI 3.14159265358979323846
+
+Monkey::Monkey(glm::vec2 position)
+{
+    SetPosition(position);
+}
+
+void Monkey::SetPosition(const glm::vec2& Position){
+    m_Transform.translation = Position;
+}
+
+void Monkey::SetImage(const std::string& ImagePath) {
+    m_ImagePath = ImagePath;
+    m_Drawable = std::make_shared<Util::Image>(m_ImagePath);
+}
+
+void Monkey::SetRadius(int radius) {
+    m_Radius = radius;
+};
+void Monkey::SetSize(glm::vec2 size){
+    m_Size = size;
+};
+void Monkey::UpdateRange() {
+    m_Range -> SetPosition(m_Transform.translation);
+    m_Range -> SetScale(m_Radius);
+};
+bool Monkey::IsInside(glm::vec2 mousePosition){
+    // 计算猴子的边界范围
+    float leftBound = m_Transform.translation.x - m_Size.x / 2.0f;
+    float rightBound = m_Transform.translation.x + m_Size.x / 2.0f;
+    float topBound = m_Transform.translation.y - m_Size.y / 2.0f;
+    float bottomBound = m_Transform.translation.y + m_Size.y / 2.0f;
+    
+    // 判断鼠标点击位置是否在猴子范围内
+    return (mousePosition.x >= leftBound &&
+            mousePosition.x <= rightBound &&
+            mousePosition.y >= topBound &&
+            mousePosition.y <= bottomBound);
+}
+
+void Monkey::CheckRangeVisible() {
+    glm::vec2 mousePosition = Util::Input::GetCursorPosition();
+    if (Util::Input::IsKeyDown(Util::Keycode::MOUSE_LB)) {
+        if (IsInside(mousePosition)) {
+            m_Range -> SetVisible(true);
+        } else {
+            m_Range -> SetVisible(false);
+        }
+    }
+};
+
+void Monkey::SetCd(int cd) {
+    m_Cd = cd;
+};
+
+std::vector<std::shared_ptr<Attack>> Monkey::ProduceAttack(glm::vec2 goalPosition){
+    SetRotation(goalPosition);
+    std::vector<std::shared_ptr<Attack>> attacks;
+    return attacks;
+}
+
+bool Monkey::IsCollision(const std::shared_ptr<Balloon>& other) const{
+    glm::vec2 position = other->GetPosition();
+    float distance = sqrt(pow(position.x - m_Transform.translation.x, 2) + pow(position.y - m_Transform.translation.y, 2));
+    return distance <= m_Radius;
+}
+
+void Monkey::SetRotation(glm::vec2 Position) {
+    double dx = Position.x - m_Transform.translation.x;
+    double dy = Position.y - m_Transform.translation.y;
+    double angle_rad = atan2(dy, dx);
+    m_Transform.rotation = angle_rad;
+}
+
+bool Monkey::Countdown() {
+    if (m_Count == m_Cd) {
+        return true;
+    }
+    m_Count += 1;
+    return false;
+}
+
+void Monkey::ResetCount(){
+    m_Count = 0;
+}
+
+// ####################################################################
+
+DartMonkey::DartMonkey(glm::vec2 position) : Monkey(position){
+    m_Transform.scale = glm::vec2(1.0f, 1.0f);
+    SetSize(glm::vec2(100.0f, 100.0f));
+    SetImage(GA_RESOURCE_DIR"/Monkey/DartMonkey.png");
+    SetCd(50);
+    SetRadius(150);
+    UpdateRange();
+}
+
+std::vector<std::shared_ptr<Attack>> DartMonkey::ProduceAttack(glm::vec2 goalPosition) {
+    ResetCount();
+    SetRotation(goalPosition);
+    std::vector<std::shared_ptr<Attack>> attacks;
+    std::shared_ptr<Attack> attack = std::make_shared<Dart>(GetPosition(), goalPosition);
+    attacks.push_back(attack);
+    return attacks;
+}
+
+//#####################################################################
+
+NailMonkey::NailMonkey(glm::vec2 position) : Monkey(position){
+    m_Transform.scale = glm::vec2(1.0f, 1.0f);
+    SetImage(GA_RESOURCE_DIR"/Monkey/NailMonkey.png");
+    SetSize(glm::vec2(100.0f, 100.0f));
+    SetCd(60);
+    SetRadius(150);
+    UpdateRange();
+}
+
+std::vector<std::shared_ptr<Attack>> NailMonkey::ProduceAttack(glm::vec2 goalPosition) {
+    ResetCount();
+    std::vector<std::shared_ptr<Attack>> attacks;
+    for (int i = 0; i < 8; i++) {
+        double theta = i * PI / 4;  // 角度轉為弧度
+        double x = m_Transform.translation.x + 1 * cos(theta);
+        double y = m_Transform.translation.y + 1 * sin(theta);
+        std::shared_ptr<Attack> attack = std::make_shared<Nail>(GetPosition(), glm::vec2(x,y), GetRadius());
+        attacks.push_back(attack);
+    }
+    return attacks;
+}
+
+//#####################################################################
+
+SniperMonkey::SniperMonkey(glm::vec2 position) : Monkey(position){
+    m_Transform.scale = glm::vec2(1.0f, 1.0f);
+    SetSize(glm::vec2(100.0f, 100.0f));
+    SetImage(GA_RESOURCE_DIR"/Monkey/SniperMonkey.png");
+    SetCd(120);
+    SetRadius(1500);
+    UpdateRange();
+}
+
+std::vector<std::shared_ptr<Attack>> SniperMonkey::ProduceAttack(glm::vec2 goalPosition) {
+    ResetCount();
+    SetRotation(goalPosition);
+    std::vector<std::shared_ptr<Attack>> attacks;
+    std::shared_ptr<Attack> attack = std::make_shared<Dart>(GetPosition(), goalPosition);
+    attack -> SetSpeed(100);
+    attack -> SetPenetration(2);
+    attacks.push_back(attack);
+    return attacks;
+}
+
+//########################################################################
+
+BoomerangMonkey::BoomerangMonkey(glm::vec2 position) : Monkey(position){
+    m_Transform.scale = glm::vec2(1.0f, 1.0f);
+    SetImage(GA_RESOURCE_DIR"/Monkey/BoomerangMonkey.png");
+    SetCd(80);
+    SetRadius(120);
+    UpdateRange();
+    SetSize(glm::vec2(100.0f, 100.0f));
+}
+
+std::vector<std::shared_ptr<Attack>> BoomerangMonkey::ProduceAttack(glm::vec2 goalPosition) {
+    ResetCount();
+    SetRotation(goalPosition);
+    std::vector<std::shared_ptr<Attack>> attacks;
+    std::shared_ptr<Attack> attack = std::make_shared<Boomerang>(GetPosition(), goalPosition);
+    attacks.push_back(attack);
+    return attacks;
+}
+
+//########################################################################
+
+NinjaMonkey::NinjaMonkey(glm::vec2 position) : Monkey(position){
+    m_Transform.scale = glm::vec2(1.0f, 1.0f);
+    SetImage(GA_RESOURCE_DIR"/Monkey/NinjaMonkey.png");
+    SetCd(30);
+    SetRadius(150);
+    UpdateRange();
+    SetSize(glm::vec2(100.0f, 100.0f));
+}
+
+std::vector<std::shared_ptr<Attack>> NinjaMonkey::ProduceAttack(glm::vec2 goalPosition) {
+    ResetCount();
+    SetRotation(goalPosition);
+    std::vector<std::shared_ptr<Attack>> attacks;
+    std::shared_ptr<Attack> attack = std::make_shared<Shuriken>(GetPosition(), goalPosition);
+    attacks.push_back(attack);
+    return attacks;
+}
+
+//########################################################################
+
+Cannon::Cannon(glm::vec2 position) : Monkey(position){
+    m_Transform.scale = glm::vec2(1.0f, 1.0f);
+    SetImage(GA_RESOURCE_DIR"/Monkey/Cannon.png");
+    SetCd(100);
+    SetRadius(160);
+    UpdateRange();
+    SetSize(glm::vec2(100.0f, 100.0f));
+}
+
+std::vector<std::shared_ptr<Attack>> Cannon::ProduceAttack(glm::vec2 goalPosition) {
+    std::vector<std::shared_ptr<Attack>> remove_attacks;
+    ResetCount();
+    SetRotation(goalPosition);
+    std::vector<std::shared_ptr<Attack>> attacks;
+    std::shared_ptr<Attack> attack1 = std::make_shared<Bomb>(GetPosition(), goalPosition);
+    attacks.push_back(attack1);
+    std::shared_ptr<Attack> attack2 = std::make_shared<Explosion>(GetPosition(), goalPosition, attack1);
+    attack2 -> SetVisible(false);
+    attacks.push_back(attack2);
+    return attacks;
+}
+
+//########################################################################
+
+Airport::Airport(glm::vec2 position) : Monkey(position){
+    m_Transform.scale = glm::vec2(1.0f, 1.0f);
+    SetImage(GA_RESOURCE_DIR"/Monkey/Airport.png");
+    SetCd(100);
+    SetRadius(20);
+    UpdateRange();
+    SetSize(glm::vec2(100.0f, 100.0f));
+}
+
+std::vector<std::shared_ptr<Attack>> Airport::ProduceAttack(glm::vec2 goalPosition) {
+    glm::vec2 position = GetPosition();
+    std::vector<std::shared_ptr<Attack>> remove_attacks;
+    ResetCount();
+    std::vector<std::shared_ptr<Attack>> attacks;
+    for (auto& airplanePtr : m_Airplanes) {
+        glm::vec2 position = airplanePtr -> GetPosition();
+        for (int i=0; i < 8; i++) {
+            glm::vec2 rotated_position = ProduceCoordinateByAngle(position, 45*i);
+            std::shared_ptr<Attack> attack = std::make_shared<Dart>(position, rotated_position);
+            attacks.push_back(attack);
+        }
+    }
+
+    if (m_Airplanes.size() < airplane_num) {
+        glm::vec2 rotated_position = ProduceCoordinateByAngle(position, 0);
+        std::shared_ptr<Attack> attack = std::make_shared<Airplane>(position, rotated_position);
+        m_Airplanes.push_back(attack);
+        attacks.push_back(attack);
+    }
+
+    return attacks;
+}
+
+bool Airport::IsCollision(const std::shared_ptr<Balloon>& other) const{
+    return true;
+}
+
+glm::vec2 Airport::ProduceCoordinateByAngle(glm::vec2 position, float angle) {
+    double radian = angle * PI / 180.0;
+    glm::vec2 rotated;
+    glm::vec2 rotated_position;
+    rotated.x = position.x * cos(radian) - position.y * sin(radian);
+    rotated.y = position.x * sin(radian) + position.y * cos(radian);
+    rotated_position.x = position.x + rotated.x;
+    rotated_position.y = position.y + rotated.y;
+    return rotated_position;
+}
